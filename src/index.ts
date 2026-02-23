f6c9d38fd35c06cab5d9e0f966120db63f548709
kwstx <kwstasgalan32@gmail.com>
2026-02-23 20:18:35 +0200

    exposing api again

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export * from './api/SelfImprovementAPI';
+export * from './api/createSelfImprovementAPI';
+
+export * from './engines/ConsensusEngine';
+export * from './engines/ImpactAssessmentEngine';
+export * from './engines/PolicyValidationLayer';
+export * from './engines/ProposalPrioritizationEngine';
+export * from './engines/ProposalSubmissionEngine';
+export * from './engines/RollbackEngine';
+export * from './engines/SandboxEvaluationEngine';
+export * from './engines/SelfImprovementFeedbackLoop';
+export * from './engines/VersionControlEngine';
+
+export * from './models/SelfModificationProposal';
